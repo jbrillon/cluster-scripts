5151229806d2d82a36974433015b6c4d48529065
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-07-06 12:47:29 -0400

    dropping cfl to 0.01

--- setup_run.sh
+++ setup_run.sh
@@ -7,7 +7,7 @@ SGS_model_type=${3}
 correction_parameter=${4}
 numerical_flux=${5}
 #---------------------------------------------------
-cfl_number="0.05"
+cfl_number="0.01"
 poly_degree="5"
 number_of_grid_elements_per_dimension="8"
 density_initial_condition_type="isothermal"
