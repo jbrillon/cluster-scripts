2fb89cf0a37c548dcc53d000dcd882f149744673
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-11-08 14:50:21 -0500

    updating generate_job_files.sh

--- generate_job_files.sh
+++ generate_job_files.sh
@@ -1,25 +1,28 @@
-filename="job_parameters_file.sh"
-time="1:00:00"
+#---------------------------------------------------
+# create job submission file
+#---------------------------------------------------
+filename="job_prm_file.sh"
+walltime="167:00:00"
 job_name="run_parameters_file"
-nodes=2
-ntasks_per_node=40
+nodes=256
+ntasks_per_node=16
 user_email="julien.brillon@mail.mcgill.ca"
 compute_canada_username="brillon"
-parameters_file="my_parameters_file.prm"
-dimension_of_problem=2
+parameters_file="input.prm"
+PHiLiP_DIM=3
 run_on_temp_dir=false
-memory_per_node=0
-
+memory_per_node="63G"
+#---------------------------------------------------
+# Generate job file
 source ./create_job_file.sh \
 ${filename} \
-${time} \
+${walltime} \
 ${job_name} \
 ${nodes} \
 ${ntasks_per_node} \
 ${user_email} \
 ${compute_canada_username} \
 ${parameters_file} \
-${dimension_of_problem} \
+${PHiLiP_DIM} \
 ${run_on_temp_dir} \
 ${memory_per_node}
-
