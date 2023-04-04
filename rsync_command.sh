3cb89ac4ce35f0a0b4c9de1d37f5a73231d1d3b5
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-04-04 16:15:00 -0400

    updating rsync command

--- rsync_command.sh
+++ rsync_command.sh
@@ -1,6 +1,3 @@
 # Ref: https://linuxize.com/post/how-to-use-rsync-for-local-and-remote-data-transfer-and-synchronization/
 # Example
-rsync -a --progress aiaa/2022-11-29_TGV_SPECTRA_24dofs brillon@narval.computecanada.ca:scratch/2023_AIAA/
-rsync -a --progress aiaa/2022-11-29_TGV_SPECTRA_48dofs brillon@narval.computecanada.ca:scratch/2023_AIAA/
-rsync -a --progress aiaa/2022-11-29_TGV_SPECTRA_96dofs_cDG_cPlus brillon@narval.computecanada.ca:scratch/2023_AIAA/
-rsync -a --progress aiaa/2022-11-29_TGV_SPECTRA_96dofs_cSD_cHU brillon@narval.computecanada.ca:scratch/2023_AIAA/
+rsync -a --progress outputs/jcp/ brillon@narval.computecanada.ca:scratch/2023_JCP/
