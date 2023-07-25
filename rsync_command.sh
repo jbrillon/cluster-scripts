753cb95efc315d3891e9f88909e66d484e481111
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-07-25 12:44:51 -0400

    updating rsync command

--- rsync_command.sh
+++ rsync_command.sh
@@ -7,10 +7,10 @@
 # rsync -a --progress outputs/jcp/sgs_model_GLL_flux_nodes brillon@narval.computecanada.ca:scratch/2023_JCP/
 # rsync -a --progress outputs/jcp/upwind_dissipation_GL_flux_nodes brillon@narval.computecanada.ca:scratch/2023_JCP/
 # rsync -a --progress outputs/jcp/upwind_dissipation_GLL_flux_nodes brillon@narval.computecanada.ca:scratch/2023_JCP/
-# rsync -a --progress outputs/jcp/verification brillon@narval.computecanada.ca:scratch/2023_JCP/
+rsync -a --progress outputs/jcp/verification/viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-4_dofs0256_p3_CFL-0.15_procs2048 brillon@narval.computecanada.ca:scratch/2023_JCP/verification/
 # rsync -a --progress outputs/jcp/filter_width_stabilization brillon@narval.computecanada.ca:scratch/2023_JCP/
 # rsync -a --progress outputs/jcp/cpu_time_advantage_2 brillon@narval.computecanada.ca:scratch/2023_JCP/
 # rsync -a --progress outputs/jcp/time_step_advantage/* brillon@narval.computecanada.ca:scratch/2023_JCP/time_step_advantage/
 # rsync -a --progress brillon@narval.computecanada.ca:scratch/2023_JCP/cpu_time_advantage_2/* outputs/jcp/cpu_time_advantage_2/
 # rsync -a --progress brillon@narval.computecanada.ca:scratch/2023_JCP/cpu_time_advantage/with_overint_comp_cost/*  outputs/jcp/cpu_time_advantage/with_overint_comp_cost/
-rsync -a --progress outputs/jcp/high_poly_degree_GL_flux_nodes/* brillon@narval.computecanada.ca:scratch/2023_JCP/high_poly_degree_GL_flux_nodes/
+# rsync -a --progress outputs/jcp/high_poly_degree_GL_flux_nodes/* brillon@narval.computecanada.ca:scratch/2023_JCP/high_poly_degree_GL_flux_nodes/
