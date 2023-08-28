5b082095f626a733c7a14c0bb6120b60906040cf
jbrillon <43619715+jbrillon@users.noreply.github.com>
2023-08-28 15:17:31 -0400

    cDG collocated vs cDG uncollocated cpu time comparison

--- create_runs.sh
+++ create_runs.sh
@@ -25,23 +25,64 @@ if [ ! -d ${base_directory} ]; then
 fi
 
 #================================================================
-# DHIT RUNS
+# cDG collocated vs cDG uncollocated cpu time comparison
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-sub_directory="${base_directory}/DHIT"
+sub_directory="${base_directory}/cpu_time_coll_vs_uncoll"
 test_directory="${sub_directory}_local_test"
-walltime="1:00:00"
-poly_degree="3"
-number_of_grid_elements_per_dimension="32"
-final_time="2.001"
-nodes=32
-is_cpu_timing_run="false"
-cfl_number="0.2"
+walltime="3:00:00"
+number_of_grid_elements_per_dimension="4"
+final_time="0.00001"
+nodes=1
+# ntasks_per_node=64
+# note: might want to use the full node
+is_cpu_timing_run="true"
+cfl_number="0.1"
 #----------------------------------------------------------------
-# cDG NSFR
-source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "DHIT" "first" ${test_directory} ${poly_degree} ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GL" "0.12" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
-# Strong DG
-over_int="4"
-source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "Roe" ${TP_FLUX_type} "DHIT" "last" ${test_directory} ${poly_degree} ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "true" ${over_int} "GL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+# poly degree min and max
+PMIN=1
+PMAX=30
+for (( p=${PMIN}; p<=${PMAX}; p++ )); do
+    poly_degree=${p}
+    # Uncollocated - NSFR cDG with IR two-point numerical flux, on GL flux nodes, n_quad=P+1 (i.e. no over-integration)
+    if [ ${p} == ${PMIN} ]; then
+        # signal first run
+        source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "TGV" "first" ${test_directory} "${poly_degree}" ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+    else
+        source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "TGV" "-" ${test_directory} "${poly_degree}" ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+    fi
+    let overintegration=${p}+1
+    # Collocated - NSFR cDG with IR two-point numerical flux, on GLL flux nodes, n_quad=P+1 (i.e. no over-integration)
+    if [ ${p} == ${PMAX} ]; then
+        # signal last run
+        source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "TGV" "last" ${test_directory} "${poly_degree}" ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GLL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+    else
+        source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "TGV" "-" ${test_directory} "${poly_degree}" ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GLL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+    fi
+done
+#----------------------------------------------------------------
+#================================================================
+
+# #================================================================
+# # DHIT RUNS
+# #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# sub_directory="${base_directory}/DHIT"
+# test_directory="${sub_directory}_local_test"
+# walltime="1:00:00"
+# poly_degree="3"
+# number_of_grid_elements_per_dimension="32"
+# final_time="2.001"
+# nodes=32
+# is_cpu_timing_run="false"
+# cfl_number="0.2"
+# #----------------------------------------------------------------
+# # cDG NSFR
+# source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "2PF" ${TP_FLUX_type} "DHIT" "first" ${test_directory} ${poly_degree} ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "false" "0" "GL" "0.12" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+# # Strong DG
+# over_int="4"
+# source ./setup_run.sh ${sub_directory} "navier_stokes" "SMAG" "cDG" "Roe" ${TP_FLUX_type} "DHIT" "last" ${test_directory} ${poly_degree} ${number_of_grid_elements_per_dimension} ${walltime} ${ntasks_per_node} ${nodes} ${memory_per_node} "true" ${over_int} "GL" "0.1" "large_eddy_simulation" ${cfl_number} ${final_time} ${is_cpu_timing_run}
+# #----------------------------------------------------------------
+# #================================================================
+
 # #================================================================
 # # (1A) FLUX NODES TYPE / BASELINE NSFR SCHEME | 96^3 DOFs
 # #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
