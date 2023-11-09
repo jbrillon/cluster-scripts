8a0c2b4040148e25b5d9969b6123e383b4c67c0d
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-11-08 20:21:21 -0500

    rsync command update for tcf

--- rsync_command.sh
+++ rsync_command.sh
@@ -1,5 +1,5 @@
 # Ref: https://linuxize.com/post/how-to-use-rsync-for-local-and-remote-data-transfer-and-synchronization/
 # Example
 # rsync -a --progress outputs/jcp/ brillon@narval.computecanada.ca:scratch/2023_JCP/
-rsync -a --progress outputs/jcp/sgs_model_GL_flux_nodes/* brillon@narval.computecanada.ca:scratch/2023_JCP/sgs_model_GL_flux_nodes/
+rsync -a --progress outputs/jcp/turbulent_channel_flow brillon@narval.computecanada.ca:scratch/2024_AIAA/
 #rsync -a --progress outputs/jcp/sgs_model_GL_flux_nodes/* brillon@narval.computecanada.ca:scratch/2023_JCP/sgs_model_GL_flux_nodes/
